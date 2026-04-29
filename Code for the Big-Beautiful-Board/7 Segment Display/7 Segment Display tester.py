db3f994f049f8ef4a040559ba9a8c9973892a38d
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-04-29 15:33:59 +0200

    Clarify LED pin assignments in 7 Segment Display tester
    Updated comments for LED pins to clarify their positions.

--- Code for the Big-Beautiful-Board/7 Segment Display/7 Segment Display tester.py
+++ Code for the Big-Beautiful-Board/7 Segment Display/7 Segment Display tester.py
@@ -2,14 +2,15 @@ from machine import Pin
 from time import sleep
 
 
-led1 = Pin(8, Pin.OUT) # led 1
-led2 = Pin(9, Pin.OUT) # led 2
-led3 = Pin(10, Pin.OUT) # led 3
-led4 = Pin(11, Pin.OUT) # led 4
-led5 = Pin(12, Pin.OUT) # led 5
-led6 = Pin(13, Pin.OUT) # led 6
-led7 = Pin(14, Pin.OUT) # led 7
-led8 = Pin(15, Pin.OUT) # led 8
+led1 = Pin(8, Pin.OUT) # led 1 - buttom 
+led2 = Pin(9, Pin.OUT) # led 2 - bottom left
+led3 = Pin(10, Pin.OUT) # led 3 - middle 
+led4 = Pin(11, Pin.OUT) # led 4 - top left
+led5 = Pin(12, Pin.OUT) # led 5 - top 
+led6 = Pin(13, Pin.OUT) # led 6 - top right
+led7 = Pin(14, Pin.OUT) # led 7 - bottum right 
+led8 = Pin(15, Pin.OUT) # led 8 - dot
+
 
 
 led_lst  = [led1, led2, led3, led4, led5, led6, led7, led8]
@@ -17,6 +18,7 @@ led_lst  = [led1, led2, led3, led4, led5, led6, led7, led8]
 while True:
     for led in led_lst:
         led.off()
+        sleep(2)
 
     for led in led_lst:
         led.on()
