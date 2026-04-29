c311d0b7057a9925b9c1199b464c121a694b5347
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-04-29 10:38:03 +0200

    Refactor variable names for 7 Segment Display

--- Code for the Big-Beautiful-Board/7 Segment Display/7 Segment Display tester.py
+++ Code for the Big-Beautiful-Board/7 Segment Display/7 Segment Display tester.py
@@ -1,18 +1,18 @@
 from machine import Pin
 from time import sleep
 
-#Thanks to CatDad for the code
 
-LedSyv1 = Pin(8, Pin.OUT) #SyvSekment led 1
-LedSyv2 = Pin(9, Pin.OUT) #SyvSekment led 2
-LedSyv3 = Pin(10, Pin.OUT) #SyvSekment led 3
-LedSyv4 = Pin(11, Pin.OUT) #SyvSekment led 4
-LedSyv5 = Pin(12, Pin.OUT) #SyvSekment led 5
-LedSyv6 = Pin(13, Pin.OUT) #SyvSekment led 6
-LedSyv7 = Pin(14, Pin.OUT) #SyvSekment led 7
-LedSyv8 = Pin(15, Pin.OUT) #SyvSekment led 8
+led1 = Pin(8, Pin.OUT) # led 1
+led2 = Pin(9, Pin.OUT) # led 2
+led3 = Pin(10, Pin.OUT) # led 3
+led4 = Pin(11, Pin.OUT) # led 4
+led5 = Pin(12, Pin.OUT) # led 5
+led6 = Pin(13, Pin.OUT) # led 6
+led7 = Pin(14, Pin.OUT) # led 7
+led8 = Pin(15, Pin.OUT) # led 8
 
-LedSyvListe = [LedSyv1, LedSyv2, LedSyv3, LedSyv4, LedSyv5, LedSyv6, LedSyv7, LedSyv8]
+
+LedSyvListe = [led1, led2, led3, led4, led5, led6, led7, led8]
 
 while True:
     for led in LedSyvListe:
