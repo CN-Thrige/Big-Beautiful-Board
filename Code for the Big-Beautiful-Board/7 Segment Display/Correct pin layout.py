b67cc72f26e2e95e7569720ab24da7fbd364a853
Christian Thrige Nielsen <149200558+CN-Thrige@users.noreply.github.com>
2026-04-30 08:15:00 +0200

    Clarify LED pin layout comments
    Updated comments for pin layout to clarify each LED's position.

--- Code for the Big-Beautiful-Board/7 Segment Display/Correct pin layout.py
+++ Code for the Big-Beautiful-Board/7 Segment Display/Correct pin layout.py
@@ -2,11 +2,15 @@
 from machine import Pin
 from time import sleep
 
-led1 = Pin(8, Pin.OUT) # led 1
-led2 = Pin(9, Pin.OUT) # led 2
-led3 = Pin(10, Pin.OUT) # led 3
-led4 = Pin(11, Pin.OUT) # led 4
-led5 = Pin(12, Pin.OUT) # led 5
-led6 = Pin(13, Pin.OUT) # led 6
-led7 = Pin(14, Pin.OUT) # led 7
-led8 = Pin(15, Pin.OUT) # led 8
+
+led1 = Pin(8, Pin.OUT) # led 1 - buttom 
+led2 = Pin(9, Pin.OUT) # led 2 - bottom left
+led3 = Pin(10, Pin.OUT) # led 3 - middle 
+led4 = Pin(11, Pin.OUT) # led 4 - top left
+led5 = Pin(12, Pin.OUT) # led 5 - top 
+led6 = Pin(13, Pin.OUT) # led 6 - top right
+led7 = Pin(14, Pin.OUT) # led 7 - bottum right 
+led8 = Pin(15, Pin.OUT) # led 8 - dot
+
+#NOTE: 
+# led8 (dot) is rarely used. 
